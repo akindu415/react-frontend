ecfd625fc41a8be555c12b71676e801145e111f4
akindu415 <akindu415@gmail.com>
2024-12-10 22:06:53 +0530

    created ticketpool status page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ConfigForm from './pages/ConfigForm';
+import TicketPoolStatus from './pages/TicketPoolStatus';
 
 function App(){
   return(
@@ -7,6 +8,7 @@ function App(){
       <Routes>
          {/* Configuration page at the root URL */}
         <Route path="/" element={<ConfigForm />} />
+        <Route path="/status" element={<TicketPoolStatus />} />
       </Routes>
     </Router> 
   );
---
+++ src/pages/TicketPoolStatus.tsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+
+function TicketPoolStatus(){
+    // data to be set from the backend.
+    //setting a place holder for now 
+
+    const [tickets, setTickets] = useState<number>(0);
+
+    useEffect(()=>{
+        //fetch the data from the backend)
+        const inerval = setInterval(()=>{
+            setTickets((prev) => prev + 1);
+        },1000);
+
+        return () => clearInterval(inerval);
+    },[]);
+
+    return(
+        <div style={{ padding: '20px' }}>
+      <h1>Ticket Pool Status</h1>
+      <p>Current Tickets in Pool: {tickets}</p>
+      <p>(This is a placeholder. You will later replace with real data from backend.)</p>
+    </div>  
+    );
+}
+
+export default TicketPoolStatus
